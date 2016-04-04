cc174004497331fbc9372ebe4a645f6d31a22943
Jonathan Parker Rogers <parker.rogers24@gmail.com>
2016-04-04 15:46:06 -0600

    the messages are on the tableView
    the current users messages are on the right. all others are on the left.
    
    no I need to change the margins

---
+++ GroupVault/CellMessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CellMessageTableViewCell.swift
+//  GroupVault
+//
+//  Created by Jonathan Rogers on 4/4/16.
+//  Copyright © 2016 Jonathan Rogers. All rights reserved.
+//
+
+import UIKit
+
+class CellMessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
